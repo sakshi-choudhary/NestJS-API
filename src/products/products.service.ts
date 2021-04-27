be150a83ff8ca6e6d7c71e8c473d884db186a096
Sakshi Choudhary <choudharysakshi023@gmail.com>
2021-04-27 16:55:28 +0530

    🚧  patch

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -17,10 +17,37 @@ export class ProductsService {
   }
 
   _getProduct(productId: string) {
-    const product = this.products.find((prod) => prod.id == productId);
+    const product = this.findProduct(productId)[0];
+    return { ...product };
+  }
+
+  _updateProduct(
+    productId: string,
+    title: string,
+    desc: string,
+    price: number,
+  ) {
+    const [product, index] = this.findProduct(productId);
+    const updatedProduct = { ...product };
+    if (title) {
+      updatedProduct.title = title;
+    }
+    if (desc) {
+      updatedProduct.desc = desc;
+    }
+    if (price) {
+      updatedProduct.price = price;
+    }
+    this.products[index] = updatedProduct;
+  }
+
+  private findProduct(id: string): [Product, number] {
+    const productIndex = this.products.findIndex((prod) => prod.id === id);
+    const product = this.products[productIndex];
     if (!product) {
       throw new NotFoundException('Opps, Could not find the product!');
     }
-    return { ...product };
+
+    return [product, productIndex];
   }
 }
